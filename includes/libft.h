560cd6fbffe4ecd6427b5c68122ecf5340e89bf1
maryvi <maryvi.jobfinder@gmail.com>
2025-10-17 23:24:37 +0100

    atoi

--- includes/libft.h
+++ includes/libft.h
@@ -35,5 +35,6 @@ int	ft_tolower(int c);
 char	*ft_strchr(const char *s, int c);
 int	ft_strncmp(const char *s1, const char *s2, size_t n);
 char	*ft_strnstr(const char *big, const char *little, size_t len);
+int	ft_atoi(const char *nptr);
 
 #endif
