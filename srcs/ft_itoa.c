fe9379e91916221837963bc1ae69910c8d3f0b82
Maryvi Camacho <macamach@c1r7s4.42porto.com>
2025-10-23 13:54:09 +0100

    itoa improved

--- srcs/ft_itoa.c
+++ srcs/ft_itoa.c
@@ -6,34 +6,22 @@
 /*   By: macamach <mcamach@student.42porto.com      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/23 09:40:36 by macamach          #+#    #+#             */
-/*   Updated: 2025/10/23 11:48:25 by macamach         ###   ########.fr       */
+/*   Updated: 2025/10/23 13:51:56 by macamach         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-int	power(int number, unsigned int exp)
-{
-	unsigned int	i;
-	int		power;
-
-	i = 0;
-	power = 1;
-	if (!number && !exp)
-		return (-1);
-	while (i < exp)
-	{
-		power *= number;
-		i++;
-	}
-	return (power);
-}
 
 unsigned int	number_len(int n)
 {
 	unsigned int	len;
 
 	len = 0;
+	if (n < 0)
+	{
+		len++;
+		n = -n;
+	}
 	while (n > 0)
 	{
 		n /= 10;
@@ -42,41 +30,31 @@ unsigned int	number_len(int n)
 	return (len);
 }
 
-unsigned int	ft_abs(int n)
-{
-	if (n > 0)
-		return (n);
-	else
-		return (-n);
-}
-
 char	*ft_itoa(int n)
 {
 	unsigned int	len;
-	unsigned int	i;
-	unsigned int	number;
-	char		*str;
-	int		digit;
+	char			*str;
 
-	number = ft_abs(n);
-	if (number == 0)
-		return ("0");
+	if (n == 0)
+		return (ft_strdup("0"));
+	if (n == -2147483648)
+		return (ft_strdup("-2147483648"));
 	len = number_len(n);
 	str = malloc(len + 1);
 	if (str == NULL)
 		return (NULL);
 	ft_memset(str, 48, len);
 	str[len] = '\0';
-	i = 0;
-	while (str[i])
-	{
-		digit = number / power(10, len - 1);
-		str[i] += digit;
-		number = number - (digit * power(10, len - 1));
-		len--;
-		i++;
-	}
 	if (n < 0)
-		return ("-1");
+	{
+		n = -n;
+		str[0] = '-';
+	}
+	while (n > 0)
+	{
+		str[len - 1] += (n % 10);
+		n = n / 10;
+		len--;
+	}
 	return (str);
 }
--- t_itoa.c
+++ t_itoa.c
@@ -4,9 +4,11 @@
 int	main(void)
 {
 	printf("str:%s\n", ft_itoa(352));
-	printf("str:%s\n", ft_itoa(8352));
+	printf("str:%s\n", ft_itoa(-8352));
 	printf("str:%s\n", ft_itoa(-1));
 	printf("str:%s\n", ft_itoa(0));
 	printf("str:%s\n", ft_itoa(1));
+	printf("str:%s\n", ft_itoa(2147483647));
+	printf("str:%s\n", ft_itoa(-2147483648));
 	return (0);
 }
