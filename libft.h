0261f28a0d93b1eb15c0d8a775cbf86ef3907aea
maryvi <mcamachog@gmail.com>
2025-10-27 23:27:53 +0000

    norminette lstnew

--- libft.h
+++ libft.h
@@ -55,8 +55,8 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size);
 
 typedef struct s_list
 {
-	void		*content;
-	struct s_list			*next;
+	void			*content;
+	struct s_list	*next;
 }	t_list;
 
 t_list	*ft_lstnew(void *content);
